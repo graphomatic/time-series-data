6d87f73410e5a03f355f9df3d88fb1fcfce113e2
Paul @ Graphomatic <paul@graphomatic.net>
2010-07-13 19:53:00 +0100

    Add the === op to TimeSeriesData::Period In the process switch to using Time internally rather than DateTime Added monkeypatch for conversion between DateTime and Time objects.

--- lib/time_series_data/period.rb
+++ lib/time_series_data/period.rb
@@ -7,6 +7,7 @@
 # Time periods are immutable
 
 require 'date'
+require 'time'
 
 class TimeSeriesData::Period
   
@@ -21,8 +22,10 @@ class TimeSeriesData::Period
   def initialize( time, duration )
     @start = case time
       when String
-        DateTime.parse( time )
-      when DateTime, Date
+        Time.parse( time )
+      when DateTime
+        time.to_time
+      when Time
         time
       else
         raise ArgumentError, "Date/DateTime object or string required."
@@ -40,6 +43,40 @@ class TimeSeriesData::Period
       
   end
   
+  def succ
+    # Get the start point of the next period.
+    successor = case @duration
+      when :minute
+        @start + 60
+      when :hour
+        @start + 60 * 60
+      when :day
+        @start + ( 60 * 60 * 24 )
+      when :week
+        @start + ( 60 * 60 * 24 * 7 )
+      when :month
+        dt = @start.to_datetime
+        dt = dt >> 1
+        dt.to_time
+      when :year
+        dt = @start.to_datetime
+        dt = dt >> 12
+        dt.to_time
+      end
+    
+    TimeSeriesData::Period.new( successor, @duration )
+  end
+  
+  def stop
+    # If the duration is :second then the start and stop are the same
+    if @duration == :second
+      return @start
+    end
+    
+    # Otherwise the stop is the second before the beginning of the next period.
+    self.succ.start - 1
+  end
+  
   # Define the <=> operator so that the Comparable mixin works
   def <=>( obj )
     if @start > obj.start
@@ -48,6 +85,29 @@ class TimeSeriesData::Period
       -1
     else
       TimeSeriesData::UNITS.index( @duration ) <=> TimeSeriesData::UNITS.index( obj.duration )
+    end
+  end
+  
+  def ===( comp )
+    puts self.start.to_s
+    puts self.stop.to_s
+    puts comp.to_s
+    
+    if comp.is_a? String then
+      comp = Time.parse( comp )
+    elsif comp.is_a? DateTime then
+      comp = comp.to_time
+    elsif comp.is_a? Time then
+      nil # That's fine...
+    else
+      raise ArgumentError, "Right hand side of TimeSeriesData::Period.=== must be either a parseable string or a DateTime"
+    end
+    puts "Start comparing"
+    if ( comp >= @start ) and
+       comp <= self.stop
+      true
+    else
+      false
     end
   end
   
---
+++ lib/time_series_data/time.rb
@@ -0,0 +1,21 @@
+# File to monkeypatch the Time and DateTime classes to make them convertable.
+# Both are useful for time calculations at differnt resolution.
+# FIX: Naive stringify/parse approach for the moment, replace with something smarter
+
+require 'date'
+require 'time'
+
+class Time
+  def to_datetime
+    DateTime.parse( self.to_s )
+  end
+end
+
+
+class DateTime
+
+  def to_time
+    Time.parse( self.to_s )
+  end
+
+end
